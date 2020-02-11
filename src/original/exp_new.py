395efdbe9af0e4c76abde7a508e9ad61a1a7c6cc
Zaccardi Jules <jules.zaccardi@ensta-paristech.fr>
2020-02-11 17:18:31 +0100

    new code

--- src/original/exp_new.py
+++ src/original/exp_new.py
@@ -20,7 +20,7 @@ import onlineFDR_proc.GAIPlus as GAIPlus
 import onlineFDR_proc.AlphaInvest as AlphaInvest
 import onlineFDR_proc.Bonferroni as Bonferroni
 
-# import 
+# import
 import rowexp_new
 from generate_mu import*
 from importme import *
@@ -38,13 +38,13 @@ def saveres(direc, filename, mat, ext = 'dat', verbose = True):
     np.savetxt(savepath, mat, fmt='%.7e', delimiter ='\t')
     if verbose:
         print("Saving results to %s" % savepath)
- 
+
 ################ Running entire framework  ####################
 
 def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma, epsilon, top_arms, alpha0, trunctimerange, FDR, NUMRUN, mu_max, alg_num = 0, punif = 0, cauchyn = 0, verbose = 1, precision = 1e-8):
     # --------- Explanation for arguments: ----
     # Sigma, epsilon are used for generating mu - sigma is the wiggle and top arms are eps away from each other
-    # Top arms: # of top arms that are around epsilon from each other 
+    # Top arms: # of top arms that are around epsilon from each other
     # ------------------------------------------
 
 
@@ -67,7 +67,7 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
     elif dist_type == 0:
         bound_type = 'Bernoulli_LIL'
 
-    # Initializing all arrays that save results per experiment (run, hypothesis and within hyp time)  
+    # Initializing all arrays that save results per experiment (run, hypothesis and within hyp time)
     pval_mat = np.zeros(shape=(numtrunc,num_hyp, NUMRUN))
     rej_mat = np.zeros(shape=(numtrunc,num_hyp, NUMRUN))
     samples_mat = np.zeros(shape=(numtrunc,num_hyp, NUMRUN))
@@ -82,7 +82,7 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
     pval_vec = np.zeros(num_hyp)
 
     #%%%%%%%%%%%%% Differnt runs of experiments %%%%%%%%%%%%%%%%%%%
-    
+
     for l in range(NUMRUN):
 
         # ---- Initialize FDR procedures, all first values are to be tossed (non-used, including alpha)
@@ -104,22 +104,22 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
 
         #%%%%%%%%%%%%%%% Run MAB for each hypothesis %%%%%%%%%%%%%%%%
         for i in range(num_hyp):
-            
+
             # Get means for the corresponding hypothesis/MAB
             mu_list = mu_mat[i]
-            this_exp = rowexp_new.rowexp(Hypo[i], no_arms, 1,  mu_list, 0, 1)
+            this_exp = rowexp_new.rowexp(Hypo[i], no_arms, 1,  mu_list, 0, sigma = 4)
 
             if verbose:
                 result_file.write("Run: %d\n" % l)
                 #result_file.write(mu_list)
 
-            # --------- Draw exp if possibly alg-dependent exp                
+            # --------- Draw exp if possibly alg-dependent exp
             this_alpha = proc.alpha[-1]
             this_exp.multi_ab(this_alpha, trunctimerange, epsilon, bound_type, alg_name, 1, cauchyn, punif, verbose = 0,  precision = precision)
             rightarm_b = this_exp.rightarm  # boolean: Whether MAB found best arm
             bestarm_idx = this_exp.bestarm['index'] #
-                
-            # -------  Compute values and all for different truncation times 
+
+            # -------  Compute values and all for different truncation times
             for q, trunctime in enumerate(trunctimerange):
                 ## Get P value at current time
                 # pval_vec[i] = this_exp.pval[q]
@@ -127,7 +127,7 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
                 # pval_mat[q][i][l] = min(this_exp.pval[0:q+1])
                 pval_mat[q][i][l] = this_exp.pval[q]
                 total_samples = this_exp.total_queries[q]
-                
+
                 samples_mat[q][i][l] = total_samples
 
                 # If wealth still positive for that procedure
@@ -141,7 +141,7 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
                     rightarm_vec[q][l] = rightarm_vec[q][l] + (Hypo[i])*rightarm_b*rej_mat[q][i][l]
                     samples_vec[q][l] = samples_vec[q][l] + total_samples
                     totrej_vec[q][l] = falrej_vec[q][l] + correj_vec[q][l]
-                    
+
                     FDR_tsr[q][i][l] = np.true_divide(falrej_vec[q][l], max(totrej_vec[q][l],1))
                     #ipdb.set_trace()
                     if verbose:
@@ -152,15 +152,15 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
                     if i < num_hyp - 1:
                         wealth_mat[q][i][l] = proc.wealth_vec[-1]
                         alpha_mat[q][i][l] = proc.next_alpha(rej_mat[q][i][l]) # use last rejection
-        
-    if verbose: 
+
+    if verbose:
         result_file.write("Time for one complete experiment with %d hypotheses was %f" % (num_hyp, time.time() - tic))
 
 
     # Save data
     dir_name = './dat'
 
-    for q, trunctime in enumerate(trunctimerange):    
+    for q, trunctime in enumerate(trunctimerange):
 
         #ipdb.set_trace()
         FDR_vec = np.true_divide(falrej_vec[q], [max(totrej_vec[q][l],1) for l in range(len(totrej_vec[q]))])
@@ -176,5 +176,5 @@ def run_single(dist_type, gap, mu_style, hyp_style, pi1, no_arms, num_hyp, sigma
         saveres(dir_name, td_filename, FDR_mat)
         saveres(dir_name, ad_filename, [BDR_vec, TDR_vec, FDR_vec, samples_vec[q], falrej_vec[q], totrej_vec[q]])
         saveres(dir_name, pr_filename, np.r_[rej_mat[q], pval_mat[q], alpha_mat[q], wealth_mat[q], samples_mat[q]])
-    
+
     result_file.close()
